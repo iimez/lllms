cf339db1fe10104b1a975a12bff6a7838c080dea
limez <limez@protonmail.com>
2024-07-15 05:26:45 +0200

    class

--- src/http.ts
+++ src/http.ts
@@ -7,9 +7,9 @@ import { createAPIMiddleware } from '#lllms/api/v1/index.js'
 import { LogLevel } from '#lllms/lib/logger.js'
 import { ModelServer, ModelServerOptions, startModelServer } from '#lllms/server.js'
 
-export function createOpenAIMiddleware(llmServer: ModelServer) {
+export function createOpenAIMiddleware(modelServer: ModelServer) {
 	const router = express.Router()
-	const requestHandlers = createOpenAIRequestHandlers(llmServer)
+	const requestHandlers = createOpenAIRequestHandlers(modelServer)
 	router.get('/v1/models', requestHandlers.models)
 	router.post('/v1/completions', requestHandlers.completions)
 	router.post('/v1/chat/completions', requestHandlers.chatCompletions)
@@ -17,42 +17,56 @@ export function createOpenAIMiddleware(llmServer: ModelServer) {
 	return router
 }
 
-export function createExpressMiddleware(llmServer: ModelServer) {
+export function createExpressMiddleware(modelServer: ModelServer) {
 	const router = express.Router()
 	router.get('/', (req, res) => {
-		res.json(llmServer.getStatus())
+		res.json(modelServer.getStatus())
 	})
-	router.use('/openai', createOpenAIMiddleware(llmServer))
-	router.use('/llama', createAPIMiddleware(llmServer))
+	router.use('/openai', createOpenAIMiddleware(modelServer))
+	router.use('/llama', createAPIMiddleware(modelServer))
 	return router
 }
 
-export interface HTTPServerOptions extends ModelServerOptions {
+export interface ModelHTTPServerOptions extends ModelServerOptions {
 	listen?: ListenOptions
 	logLevel?: LogLevel
 }
 
-export async function startHTTPServer(
-	options: HTTPServerOptions,
-) {
-	const { listen, ...serverOpts } = options
-	const listenOpts = listen ?? { port: 3000 }
-	const llms = new ModelServer(serverOpts)
-	await llms.start()
-	const app = express()
-	app.use(
-		cors(),
-		express.json({ limit: '50mb' }),
-		createExpressMiddleware(llms),
-	)
+export class ModelHTTPServer {
+	httpServer: http.Server
+	modelServer: ModelServer
+	listenOptions: ListenOptions
+	
+	constructor(options: ModelHTTPServerOptions) {
+		const { listen, ...modelServerOpts } = options
+		this.modelServer = new ModelServer(modelServerOpts)
+		this.listenOptions = listen ?? { port: 3000 }
+		const app = express()
+		app.use(
+			cors(),
+			express.json({ limit: '50mb' }),
+			createExpressMiddleware(this.modelServer),
+		)
+	
+		app.set('json spaces', 2)
+		this.httpServer = http.createServer(app)
+	}
+	
+	async start() {
+		await this.modelServer.start()
+		await new Promise<void>((resolve) => {
+			this.httpServer.listen(this.listenOptions, resolve)
+		})
+	}
+	
+	async close() {
+		this.httpServer.close()
+		await this.modelServer.stop()
+	}
+}
 
-	app.set('json spaces', 2)
-	const httpServer = http.createServer(app)
-	httpServer.on('close', () => {
-		llms.stop()
-	})
-	await new Promise<void>((resolve) => {
-		httpServer.listen(listenOpts, resolve)
-	})
-	return httpServer
+export async function startHTTPServer(options: ModelHTTPServerOptions) {
+	const server = new ModelHTTPServer(options)
+	await server.start()
+	return server
 }
